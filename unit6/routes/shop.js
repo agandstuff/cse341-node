b2a04511ee1f9d4e7d7e0e6e6eef8d7f4cd50e49
Use your GitHub username <Enter your email address>
2022-01-15 01:43:17 -0700

    update

---
+++ unit6/routes/shop.js
@@ -0,0 +1,24 @@
+//const path = require('path');
+
+const express = require('express');
+
+const router = express.Router();
+
+//const rootDir = require('../util/path')
+
+const adminData = require('./admin');
+
+router.get('/', (req, res, next) => {
+    const products = adminData.products;
+    res.render('shop', {
+        prods: products, 
+        pageTitle: 'My Shop', 
+        path: '/', 
+        hasProducts: products.length > 0, 
+        productCSS: true, 
+        activeShop: true
+        //, layout: false * Key that states you don't want the default layout * 
+    });
+});
+
+module.exports = router;
