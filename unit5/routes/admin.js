ed576bf12ea3898e782f18f91d7a415ea3ecc1db
Use your GitHub username <Enter your email address>
2022-01-14 19:42:35 -0700

    update

--- unit5/routes/admin.js
+++ unit5/routes/admin.js
@@ -4,9 +4,11 @@ const express = require('express');
 
 const router = express.Router();
 
+const rootDir = require('../util/path');
+
 //routes below are accessed through /admin/add-product (=> get) & /admin/product (=> POST)
 router.get('/add-product', (req, res, next) => {
-    res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'));
+    res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
 });
 
 //.post will only respond to post requests
