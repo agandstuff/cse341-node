7e490fb94280c9e623ff39cf299a02a44df81864
Use your GitHub username <Enter your email address>
2022-01-22 21:48:10 -0700

    Deploy to Heroku

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const path = require('path');
 const PATH = process.env.PORT || 5000;
+
 const express = require('express');
 const bodyParser = require('body-parser');
-// const expressHbs = require('express-handlebars');
-
-const app = express();
 
 const errorController = require('./controllers/error');
+
+const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -14,7 +14,7 @@ app.set('views', 'views');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/admin', adminRoutes);
